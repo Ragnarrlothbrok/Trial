ed7dc8fa11a55ccf8235eb5a294c04faed66cae1
Ragnarrlothbrok <42968641+Ragnarrlothbrok@users.noreply.github.com>
2020-11-08 23:34:50 +0530

    Trying again

--- models/Admins.js
+++ models/Admins.js
@@ -11,6 +11,10 @@ const AdminSchema = new schema({
     type: String,
     required: true,
   },
+  email3: {
+    type: String,
+    required: true,
+  },
   email2: {
     type: String,
     required: true,
